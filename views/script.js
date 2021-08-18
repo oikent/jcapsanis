52bac3fa57076f2957e65a2c6bb25a0b6f8a568a
oikent <christianstickel@hotmail.com>
2021-08-18 20:53:48 +1000

    added functionality and responsiveness

--- views/script.js
+++ views/script.js
@@ -1,18 +1,82 @@
-const scrollbarPosition = 1200;
-const scrollbarPositionTablet = 900;
-const services = document.querySelector('.services');
+// Slide up animation when scrolling
+const targets = document.querySelectorAll('section');
+
+let options = {
+    root: null,
+    threshold: 0.1
+}
+const slideUp = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const target = entry.target;
+                target.classList.add('animation-slide-up');
+                target.style.opacity = 1;
+                observer.disconnect();
+            }
+            else {
+                target.style.opacity = 0;
+            }
+        });
+    }, options);
+    io.observe(target)
+};
 
 
+targets.forEach(slideUp)
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= scrollbarPositionTablet && window.innerWidth > 359 && window.innerWidth < 1500) {
-        console.log('Tablet');
-        services.classList.add('animation');
+// hamburger click event
+
+const hamburger = document.querySelector('.hamburger');
+
+const nav = document.querySelector('nav>ul');
+
+hamburger.addEventListener('click', () => {
+    if (hamburger.className == 'fas fa-bars hamburger') {
+        hamburger.classList.toggle('open');
+        nav.style.display = 'block';
+        nav.style.animation = 'slide-right 0.5s ease'
     }
-    else if (window.scrollY >= scrollbarPosition) {
-        console.log('Desktop and mobile')
-        services.classList.add('animation');
+    else {
+        hamburger.classList.toggle('open');
+        nav.style.animation = 'slide-off 0.5s ease forwards'
+        // nav.style.display = 'none';
 
     }
+
 })
+// add click event to ul of hamburger menu to slide off screen and close menu
+nav.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    nav.style.animation = 'slide-off 0.5s ease forwards';
+})
+
+// image gallery
+
+// change image src every 5 seconds runs indefinatly because setTimeout calls the function it is in
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000);
+}
+
+
+
 
