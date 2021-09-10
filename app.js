8be6996e9f866f649f7d598ddb7c7eb148a46f56
oikent <41896789+oikent@users.noreply.github.com>
2021-09-10 23:16:36 +1000

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.use(express.static(__dirname + '/views'));
 // app.get('/', function (req, res) {
